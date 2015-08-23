15d45e2f34cb839567b159e83e81cd1f3d7974f8
Sunave <antti.suniala@gmail.com>
2015-08-23 21:16:59 +0300

    Add day cycle

---
+++ Assets/Scripts/DayCycle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayCycle : MonoBehaviour {
+
+	[Tooltip ("Number of minutes per second that pass, try 60")]
+	public float minutesPerSecond = 60f;
+
+	private Quaternion startRotation;
+
+	void Start () {
+		startRotation = transform.rotation;
+	}
+	
+	void Update () {
+		float angleThisFrame = Time.deltaTime / 360 * minutesPerSecond;
+		transform.RotateAround (transform.position, Vector3.forward, angleThisFrame);
+	}
+}
