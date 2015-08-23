1b8122a9f3d5eafd284b5396ab332e4098371cfa
Sunave <antti.suniala@gmail.com>
2015-08-23 13:48:40 +0300

    Add spawn points for player

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public bool respawn;
+
+	private Transform[] spawnPoints;
+
+	void Start () {
+		spawnPoints = GameObject.Find("PlayerSpawnPoints").GetComponentsInChildren<Transform>();
+	}
+	
+	void Update () {
+		print(spawnPoints.Length);
+		if (respawn) ReSpawn();
+	}
+
+	private void ReSpawn() {
+		int i = Random.Range(1, spawnPoints.Length);
+		transform.position = spawnPoints[i].transform.position;
+		respawn = false;
+	}
+}
